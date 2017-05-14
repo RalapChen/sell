d0bd591c8e9f3a9e4b51eb7be4094e11f5ba59e6
RalapChen <340337146@qq.com>
2017-05-15 01:32:46 +0800

    第一次导入

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item"><router-link to="/goods">商品</router-link></div>
+      <div class="tab-item"><router-link to="/goods">评论</router-link> </div>
+      <div class="tab-item"><router-link to="/goods">商家</router-link></div>
+    </div>
+    <router-view></router-view>
+    <p>啊啊啊</p>
+    <div>
+      222222222222222222222
+      <div class="tab">
+        <div class="tab-item"><router-link to="/goods">商品</router-link></div>
+        <div class="tab-item"><router-link to="/goods">评论</router-link> </div>
+        <div class="tab-item"><router-link to="/goods">商家</router-link></div>
+      </div>
+      <router-view></router-view>
+      <p>啊啊啊</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header.vue'
+export default {
+  components: {
+    'v-header':header
+  }
+}
+</script>
+
+<style  lang="scss" rel="stylesheat/sass">
+    .tab{
+      display: flex;
+      width: 100%;
+      height: 40px;
+      line-height: 40px;
+    }
+    .tab-item{
+      flex: 1;
+      text-align: center;
+    }
+</style>
---
+++ src/main.js
@@ -0,0 +1,48 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+/*import router from './router'*/
+import VueRouter from 'vue-router'
+import goods from './components/goods/goods.vue'
+
+Vue.use(VueRouter)
+Vue.config.productionTip = false
+
+/*let app=Vue.extend(App)*/
+
+/*let  router=new VueRouter()
+
+router.map({
+  '/goods':{
+    componment:goods
+  }
+})
+
+router.start(app,"#app")*/
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    {
+    path: '/goods',
+    component: goods
+    }
+    /* ,
+    {
+      path: '/seller',
+      component: seller
+    },
+    {
+      path: '/ratings',
+      component: ratings
+    }*/
+  ]
+})
+
+// 现在我们可以启动应用了！
+// 路由器会创建一个 App 实例，并且挂载到选择符 #app 匹配的元素上。
+let app = new Vue({
+    router: router,
+    render: h => h(App)
+}).$mount('#app')
